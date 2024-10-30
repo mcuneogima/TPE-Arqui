f7a05123325c55e43881c2cd487cc383bd841969
mcuneogima <mcuneogima@itba.edu.ar>
2024-10-30 19:03:36 -0300

    Cambios para aceptar sonido

--- compilatodo.sh
+++ compilatodo.sh
@@ -3,4 +3,5 @@ docker exec -it TPE-Arqui make clean -C /root/Toolchain
 docker exec -it TPE-Arqui make clean -C /root
 docker exec -it TPE-Arqui make -C /root/Toolchain
 docker exec -it TPE-Arqui make -C /root
+docker exec -it TPE-Arqui chmod 777 /root/Image/x64BareBonesImage.qcow2
 docker stop TPE-Arqui
