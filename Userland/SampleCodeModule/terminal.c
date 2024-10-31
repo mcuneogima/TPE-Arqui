40706a75037d0b425746a9a28b4fb0d5a8e497c0
mcuneogima <mcuneogima@itba.edu.ar>
2024-10-31 08:06:19 -0300

    Correccion borrado de registros por comando

--- Userland/SampleCodeModule/terminal.c
+++ Userland/SampleCodeModule/terminal.c
@@ -89,7 +89,7 @@ void terminal(){
                 lastRunHeight=0;
             }else if(!strcmp(buffer,"imprimirRegistros")){
                 imprimirRegistros();
-                lastRunHeight=10;
+                lastRunHeight=13;
             }else if(!strcmp(buffer,"exit")){
                 print("Bye Bye");
                 sleepUser(20);
@@ -147,7 +147,7 @@ void clean(int ammount){
 
 
 void help(){
-    print("\n\nBienvenido a la terminal\n Comandos disponibles: \n \tzoom in para agrandar la letra \n \tzoom out para achicar la letra\n \tclock para ver la hora actual\n \tImprimirRegistros para mostrar los registros en pantalla\n \tsnake para jugar al juego de la viborita \n ");
+    print("\n\nBienvenido a la terminal\n Comandos disponibles: \n \tzoom in para agrandar la letra \n \tzoom out para achicar la letra\n \tclock para ver la hora actual\n \timprimirRegistros para mostrar los registros en pantalla\n \tsnake para jugar al juego de la viborita \n ");
 }
 
 void refreshScreen(){
